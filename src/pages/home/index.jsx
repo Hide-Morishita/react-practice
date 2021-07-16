fa19ecf0ef22ecdd91f7f6b85c749cdb8ae86c11
Hide-Morishita <hide.morishita@di-v.co.jp>
2021-07-16 09:35:10 +0900

    じゃんけん機能準備

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,7 @@
 import { findByLabelText } from "@testing-library/dom";
 import React from "react";
+// Linkを使用するための記述
+import { Link } from "react-router-dom";
 
 // クラスコンポーネント
 // 他のファイルで呼びだすために、"export default"を記述している
@@ -14,6 +16,9 @@ export default class Index extends React.Component {
         }}
       >
         <h1>ジャンケンに挑戦しよう！</h1>
+        <Link to="/rsp">
+          <h1>じゃんけんページへ</h1>
+        </Link>
       </div>
     );
   }
--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+// Linkを使用するための記述
+import { Link } from "react-router-dom";
 
 export default class Index extends React.Component {
   render() {
@@ -11,7 +13,9 @@ export default class Index extends React.Component {
         }}
       >
         <h1>じゃんけんページ</h1>
-        <h1>じゃんけんを終了する</h1>
+        <Link to="/">
+          <h1>じゃんけんを終了する</h1>
+        </Link>
       </div>
     );
   }
