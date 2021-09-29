262ecc458b8c275da3a072c3031faacd5eb57f94
Hide-Morishita <hide.morishita@di-v.co.jp>
2021-09-29 10:17:27 +0900

    じゃんけん

--- src/pages/rsp/index.jsx
+++ src/pages/rsp/index.jsx
@@ -3,7 +3,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default class Index extends React.Component {
+  constructor() {
+    // constructorは、Rubyでいうinitializeメソッド
+    // 初期値の設定時には必ずsuperを記述する
+    super();
+    this.state = {
+      // state値は、RubyでいうHash、キーとバリューで記述する
+      selectHand: 0,
+    };
+  }
   render() {
+    // cssで、画像サイズのプロパティの値に利用
+    const imgSize = 100;
     return (
       <div
         style={{
@@ -13,8 +24,53 @@ export default class Index extends React.Component {
         }}
       >
         <h1>じゃんけんページ</h1>
+        <div style={{ display: "flex" }}>
+          <button
+            onClick={ () => {
+              console.log("グーがクリックされました");
+            }}
+          >
+            <img
+              src={`images/rock.jpeg`}
+              alt="rock"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button
+            onClick={ () => {
+              console.log("パーがクリックされました");
+            }}
+          >
+            <img
+              src={`images/paper.jpeg`}
+              alt="paper"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+          <button
+            onClick={ () => {
+              console.log("チョキがクリックされました");
+            }}
+          >
+            <img
+              src={`images/scissors.jpeg`}
+              alt="scissors"
+              style={{
+                height: imgSize,
+                width: imgSize,
+              }}
+            />
+          </button>
+        </div>
         <Link to="/">
           <h1>じゃんけんを終了する</h1>
+          <h4>※戦績が発表されます。</h4>
         </Link>
       </div>
     );
--- src/routing.jsx
+++ src/routing.jsx
@@ -11,7 +11,7 @@ export default class Routing extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exapt path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route path="/rsp" component={Rsp} />
       </BrowserRouter>
     );
